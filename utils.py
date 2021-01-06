6a8fc786e5d89c3faf5d8e55574a2c522c8f0d89
Sarthak Kamat <sarthakkamat@gmail.com>
2021-01-06 09:49:03 -0800

    dice

--- utils.py
+++ utils.py
@@ -2,33 +2,17 @@ import torch
 import torch
 import torch.nn as nn
 from model import DAVNet2D
-import numpy as np
 
-def dice_loss(input, target):
-    eps = 0.000001
-    _, result_ = input.max(1)
-    result_ = torch.squeeze(result_)
-    if input.is_cuda:
-        result = torch.cuda.FloatTensor(result_.size())
-        target_ = torch.cuda.FloatTensor(target.size())
-    else:
-        result = torch.FloatTensor(result_.size())
-        target_ = torch.FloatTensor(target.size())
-    result.copy_(result_.data)
-    target_.copy_(target.data)
-    target = target_
-    intersect = torch.dot(result, target)
+def dice_loss(Y_hat, Y):
+    assert Y_hat.size() == Y.size()
+    Y_hat = torch.flatten(Y_hat, start_dim=1)
+    Y = torch.flatten(Y, start_dim=1)
+    b, L = Y.size(0), Y.size(1)
 
-    result_sum = torch.sum(result)
-    target_sum = torch.sum(target)
-    union = result_sum + target_sum + 2*eps
-    intersect = np.max([eps, intersect])
-    # the target volume can be empty - so we still want to
-    # end up with a score of 1 if the result is 0/0
-    IoU = intersect / union
-    #    print('union: {:.3f}\t intersect: {:.6f}\t target_sum: {:.0f} IoU: result_sum: {:.0f} IoU {:.7f}'.format(
-    #        union, intersect, target_sum, result_sum, 2*IoU))
-    return 2 * IoU
+    I = 2 * torch.bmm(Y.view(b, 1, L), Y_hat.view(b, L, 1))
+    U = torch.dot(Y.view(b, 1, L), Y.view(b, 1, L)) +
+        torch.dot(Y_hat.view(b, 1, L), Y_hat.view(b, 1, L))
+    return I/U
 
 default_configs = {
     'balanced_batch_size': 8,
