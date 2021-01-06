a781a50b58707bcbfa0b651e3ce2810a486ca223
Sarthak Kamat <sarthakkamat@gmail.com>
2021-01-06 10:44:40 -0800

    bug fixes

--- train.py
+++ train.py
@@ -22,12 +22,12 @@ def train(**kwargs):
     n = configs['num_workers']
     dataloaders = {
         'balanced': {
-            'train': DataLoader(dataset=kMRI('train', balanced=True, group='all'), batch_size=configs['balanced_batch_size'], shuffle=True, num_workers=4),
-            'valid': DataLoader(dataset=kMRI('valid', balanced=True, group='all'), batch_size=configs['balanced_batch_size'], shuffle=True, num_workers=4)
+            'train': DataLoader(dataset=kMRI('train', balanced=True, group='all'), batch_size=configs['balanced_batch_size'], shuffle=True),
+            'valid': DataLoader(dataset=kMRI('valid', balanced=True, group='all'), batch_size=configs['balanced_batch_size'], shuffle=True)
         },
         'all_source': {
-            'train': DataLoader(dataset=kMRI('train', balanced=False, group='source'), batch_size= configs['all_source_batch_size'], shuffle=True, num_workers=4),
-            'valid': DataLoader(dataset=kMRI('valid', balanced=False, group='source'), batch_size= configs['all_source_batch_size'], shuffle=True, num_workers=4)
+            'train': DataLoader(dataset=kMRI('train', balanced=False, group='source'), batch_size= configs['all_source_batch_size'], shuffle=True),
+            'valid': DataLoader(dataset=kMRI('valid', balanced=False, group='source'), batch_size= configs['all_source_batch_size'], shuffle=True)
         }
     }
 
