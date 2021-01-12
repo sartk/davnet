a6be1b31ccda8605cdf52bf9a5fb7d30d7925be7
Sarthak Kamat <sarthakkamat@gmail.com>
2021-01-11 21:18:49 -0800

    err fix

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def dice_loss(Y_hat, Y, smooth=1e-10, save=False):
     intersection = (Y * Y_hat).sum(1)
     union = Y.sum(1) + Y_hat.sum(1)
     dice = (2 * intersection + smooth) / (union + smooth)
-    return -torch.log(dice)
+    return -torch.log(dice.sum())
 
 default_configs = {
     'balanced_batch_size': 8,
