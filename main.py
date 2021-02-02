cf4f769ae234ef5d5bc9c170f0a4dfc474514e6a
Sarthak Kamat <sarthakkamat@gmail.com>
2021-02-01 16:07:25 -0800

    warmup length

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import train as trainer
 import sys
-trainer.train(CUDA_VISIBLE_DEVICES=sys.argv[1], all_source_epoch=int(sys.argv[3]), checkpoint=sys.argv[2])
+trainer.train(CUDA_VISIBLE_DEVICES=sys.argv[1], warmup_length=int(sys.argv[3]), checkpoint=sys.argv[2])
